ba05749ab5f7c271596c9e45d5dcc6d09370f6be
thpark01 <67967001+tpark2001@users.noreply.github.com>
2023-02-28 01:07:03 -0500

    Initial Commit

---
+++ Bored/BoredApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoredApp.swift
+//  Bored
+//
+//  Created by Timothy Park on 2/28/23.
+//
+
+import SwiftUI
+
+@main
+struct BoredApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
